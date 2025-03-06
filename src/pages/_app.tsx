7c251c7c49ae42e7da43b3ae19b95c48337f4346
minhhieu298 <hieuti2908@gmail.com>
2025-03-06 20:54:41 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js', { scope: '/' })
+        .then((registration) => {
+          console.log(
+            'Service worker registered successfully. Scope:',
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.error('Service worker registration failed:', error);
+        });
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
