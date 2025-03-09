fb09febc94bd462c7339884a314750f1f6361960
minhhieu298 <hieuti2908@gmail.com>
2025-03-09 11:40:57 +0700

    update code

--- apps/Customer/next.config.js
+++ apps/Customer/next.config.js
@@ -12,6 +12,34 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  compress: true,
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 1000000, // Tăng lên 500KB để giảm số chunk
+      minSize: 500000, // Đảm bảo chunk đủ lớn để hợp lý
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          priority: 10,
+        },
+      },
+    };
+    config.module.rules.push({
+      test: /\.(woff|woff2|ttf|eot|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/fonts/',
+          publicPath: '/_next/static/fonts/',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 const plugins = [
--- apps/Customer/pages/_app.tsx
+++ apps/Customer/pages/_app.tsx
@@ -1,14 +1,48 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { loadFonts } from '@module-federation-next/fonts';
+import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
+
+const LoadFontsComponent = () => {
+  useEffect(() => {
+    loadFonts();
+  }, []); // Chỉ chạy một lần khi component mount trên client
+
+  return null; // Component không render gì cả, chỉ chạy logic
+};
+
+// Sử dụng dynamic với ssr: false
+const LoadFonts = dynamic(() => Promise.resolve(LoadFontsComponent), {
+  ssr: false, // Chạy chỉ trên client
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js', { scope: '/' })
+        .then((registration) => {
+          console.log(
+            'Service worker registered successfully. Scope:',
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.error('Service worker registration failed:', error);
+        });
+    }
+  }, []);
+  
   return (
     <>
       <Head>
         <title>Welcome to customer!</title>
       </Head>
       <main className="app">
+        {/* <LoadFonts /> */}
         <Component {...pageProps} />
       </main>
     </>
